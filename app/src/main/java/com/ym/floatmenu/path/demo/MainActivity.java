a76cfdd075fd640856b23b2c10ec1271ebffd0eb
Q <foldervapp@gmail.com>
2023-04-09 08:58:41 +0800

    m

--- app/src/main/java/com/ym/floatmenu/path/demo/MainActivity.java
+++ app/src/main/java/com/ym/floatmenu/path/demo/MainActivity.java
@@ -3,7 +3,7 @@ package com.ym.floatmenu.path.demo;
 import com.ym.floatmenu.path.PathMenu;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 
